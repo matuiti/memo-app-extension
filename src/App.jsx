98bedc65254aeea3df6e16dc6110ee54d718a3e6
matuiti <ichi.animo@gmail.com>
2025-08-29 21:52:42 +0900

    簡単にコンポーネントと状態を設計

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 
 function App() {
+  // 管理する状態（予定）
+  // memos: [{id,title,content}]
+  // selectedMemoId = null
+  // editedContetnt = '編集中の内容'
 
+  // ローカルストレージ連携
+  // useEffectで初回読み込み時にlocalStorageからmemosを取得
+  // memosが更新されるたびにlocalStorageに保存
   return (
     <>
-    aa
+    {/* MemoList */}
+    {/* AddBtn */}
+    {/* DeleteBtn */}
+    {/* MemoEditor */}
     </>
   )
 }
